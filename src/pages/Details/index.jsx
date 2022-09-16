5ffd66e05635d5c27c3d8fd94b072e493ffd2dda
dsllt <dayanesallet@hotmail.com>
2022-09-16 04:37:26 -0300

    first commit

---
+++ src/pages/Details/index.jsx
@@ -0,0 +1,18 @@
+import { Container } from "./styles"
+
+import { Header } from "../../components/Header"
+import { Button } from "../../components/Button"
+
+export function Details(){
+  
+  return(
+    <Container>
+      <Header>
+        
+      </Header>
+      <h1>Hello World!</h1>
+
+      <Button title = "Voltar"/>
+    </Container>
+  )
+}
